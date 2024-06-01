1e9d9a3184449ffa5db354c18fcf299b6a167741
amuller18 <97063025+amuller18@users.noreply.github.com>
2024-06-01 18:12:29 -0400

    g

--- test_data_alg.py
+++ test_data_alg.py
@@ -3,7 +3,8 @@ from analyzer import Analyzer
 file_path = 'output.csv'
 
 analyzer = Analyzer()
-analyzer.train_model()
+
 data = pd.read_csv(file_path)
-data['Post Text'].apply(lambda x: print(f'\n{analyzer.predict(x)} is the label for {x}'))
-#data.apply(lambda x: print(x['Post Text']))
+texts = data['Post Text']
+embeddings = [analyzer.get_embedding(text) for text in texts]
+reduced_embeddings = analyzer.reduce(embeddings)
